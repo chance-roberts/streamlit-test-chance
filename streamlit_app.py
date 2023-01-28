e20d44815e3697c30040b2ba692aa6d355d2d383
Chance Roberts <cjr6d@mtmail.mtsu.edu>
2023-01-28 15:31:47 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,11 +7,11 @@ con.write("Inside container")
 
 #Designer function to memorize dataframe display.
 #@st.experimental_memo #(show_spinner=True)
-def load_data():
-    return pd.read_csv("data/reddit_comments.csv")
+#def load_data():
+    #return pd.read_csv("data/reddit_comments.csv")
 
 #Save load_data function into df var. access df in streamlit.
-df = load_data()
+#df = load_data()
 
 
 #Side bar area.
