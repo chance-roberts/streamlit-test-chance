e344abcfdb9a4dd1f023a5f80a1bbdd35f000526
Chance Roberts <cjr6d@mtmail.mtsu.edu>
2023-01-28 20:38:38 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,8 @@ with st.sidebar:
         url = abt[i+1][0]
         st.image(url)
         #name
-        caption = abti=[i+1][1]
+        caption = abt=[i+1][1]
+        st.caption(caption)
         #for j in range(len(abt)-1):
             #caption = abt[i+1][j+1]
             #st.caption(caption)
